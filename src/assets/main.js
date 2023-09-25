1455874ec5cd3572b854249ed06d682f50dfe60e
omarhernandezrey <hernandezreyomar@gmail.com>
2023-09-25 09:19:07 -0500

    30 Consumiendo API

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,45 +1,44 @@
-const API = 'https://youtube-v31.p.rapidapi.com/captions?part=%40omarhernandezrey7315%2Ffeatured&videoId=M7FIvfx5J10';
-
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
 
 const content = null || document.getElementById('content');
+
 const options = {
-	method: 'GET',
-	headers: {
-		'X-CACHEBYPASS': 'youtube-v31.p.raoidapi.com',
-		'X-RapidAPI-Key': '730e7b25damsh800786d28ec1b12p1781f4jsne9b94d6d776c',
-		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
+  }
 };
 
 async function fetchData(urlApi) {
-    const response = await fetch(urlApi, options);
-	const data = await response.json();
-    return data;
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
 }
 
 (async () => {
   try {
     const videos = await fetchData(API);
-      let view = `
-      ${videos.items.map(video => `
+    let view = `
+    ${videos.items.map(video => `
       <div class="group relative">
-      <div
-      class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-      <img src="${video.snippet.thumbnail.hight.url}" alt="${video.snippet.description}" class="w-full">
-      </div>
-      <div class="mt-4 flex justify-between">
-      <h3 class="text-sm text-gray-700">
-          <span aria-hidden="true" class="absolute inset-0"></span>
-          ${video.snippet.title}
-      </h3>
-      </div>
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
       </div>
     `).slice(0,4).join('')}
- 
+
     `;
   } catch {
-    
-  } 
+
+  }
 })();
 
 
