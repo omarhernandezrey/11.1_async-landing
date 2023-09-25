c04cdd6dba7a31b648f7e42d13ca340e73000c14
omarhernandezrey <hernandezreyomar@gmail.com>
2023-09-25 11:05:27 -0500

    31 Desplegando el proyecto

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,5 @@
 const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
-
-const content = null || document.getElementById('content');
+const content = document.getElementById('content'); // Obtén el elemento con el ID 'content'
 
 const options = {
   method: 'GET',
@@ -11,41 +10,41 @@ const options = {
 };
 
 async function fetchData(urlApi) {
-  const response = await fetch(urlApi, options);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null; // En caso de error, devuelve null
+  }
 }
 
 (async () => {
   try {
     const videos = await fetchData(API);
+    
+    // Genera el HTML para mostrar los videos
     let view = `
-    ${videos.items.map(video => `
-      <div class="group relative">
-        <div
-          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+      ${videos.items.map(video => `
+        <div class="group relative">
+          <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${video.snippet.title}
+            </h3>
+          </div>
         </div>
-        <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-gray-700">
-            <span aria-hidden="true" class="absolute inset-0"></span>
-            ${video.snippet.title}
-          </h3>
-        </div>
-      </div>
-    `).slice(0, 4).join('')}
+      `).slice(0, 4).join('')}
     `;
+    
+    // Inserta el HTML generado en el elemento 'content'
     content.innerHTML = view;
   } catch (error) {
     console.log(error);
   }
 })();
 
-
-/* try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-} */
