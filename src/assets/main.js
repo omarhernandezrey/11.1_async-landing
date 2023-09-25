cd58ccfa94c1de20a99f157a45e144e04ea7bd43
omarhernandezrey <hernandezreyomar@gmail.com>
2023-09-25 12:45:14 -0500

    Mi pagina

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,5 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
+const channelId = 'UCTZFAzSm3V3Mt3v1Xqv9Oxg'; // Reemplaza con tu ID de canal
+const API = `https://youtube-v31.p.rapidapi.com/search?channelId=${channelId}&part=snippet%2Cid&order=date&maxResults=9`;
 const content = document.getElementById('content'); // Obtén el elemento con el ID 'content'
 
 const options = {
@@ -23,11 +24,11 @@ async function fetchData(urlApi) {
 (async () => {
   try {
     const videos = await fetchData(API);
-    
+
     // Genera el HTML para mostrar los videos
     let view = `
       ${videos.items.map(video => `
-        <div class="group relative">
+        <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank" class="group relative">
           <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
             <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
           </div>
@@ -37,16 +38,14 @@ async function fetchData(urlApi) {
               ${video.snippet.title}
             </h3>
           </div>
-        </div>
+        </a>
       `).slice(0, 4).join('')}
     `;
-    
+
     // Inserta el HTML generado en el elemento 'content'
     content.innerHTML = view;
   } catch (error) {
     console.log(error);
   }
 })();
-
-
 
